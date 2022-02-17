c2b5221300363f743322030f59a2a73facdbe803
chelmann <chelmann@psrc.org>
2022-02-16 16:05:12 -0800

    function for ofm postcensal data

---
+++ resources/get-ofm-april.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+
+get_ofm_postcensal_population <- function (ofm.url="https://ofm.wa.gov/sites/default/files/public/dataresearch/pop/april1/hseries/ofm_april1_postcensal_estimates_pop_1960-present.xlsx", counties = c("King","Kitsap","Pierce","Snohomish")) {
+  
+  download.file(ofm.url, "working.xlsx", quiet = TRUE, mode = "wb")
+  ofm.pop.file <- paste0(getwd(),"/working.xlsx")
+  ofm <- readxl::read_excel(ofm.pop.file, sheet="Population",skip=3)
+  file.remove(ofm.pop.file)
+  
+  ofm <- ofm %>% 
+    dplyr::filter(County %in% counties) %>%
+    dplyr::select(!(dplyr::contains("."))) %>%
+    dplyr::select(-Line) %>%
+    tidyr::pivot_longer(cols=dplyr::contains("Population"), names_to="Variable", values_to="Estimate") %>%
+    dplyr::filter(Estimate != ".") %>%
+    dplyr::mutate(Estimate = as.numeric(Estimate)) %>%
+    dplyr::mutate(Variable = gsub("\r\n","",Variable)) %>%
+    dplyr::mutate(Year = substr(Variable, 1, 4)) %>%
+    dplyr::mutate(Year = as.numeric(Year)) %>%
+    dplyr::filter(!(Variable %in% c("1970 Postcensal Estimate of Total Population","1980 Postcensal Estimate of Total Population",
+                                    "1990 Postcensal Estimate of Total Population","2000 Postcensal Estimate of Total Population",
+                                    "2010 Postcensal Estimate of Total Population","2020 Postcensal Estimate of Total Population"))) %>%
+    dplyr::mutate(Variable = stringr::str_sub(Variable, 6, stringr::str_length(Variable)))
+  
+  # Create a Regional Summary by Filter Type and then Join to Full OFM tibble
+  region <- ofm %>%
+    dplyr::filter(Filter <= 3) %>%
+    dplyr::select(Filter, Variable, Year, Estimate) %>%
+    dplyr::group_by(Filter, Variable, Year) %>%
+    dplyr::summarize_all(sum) %>%
+    dplyr::mutate(County = "Region") %>%
+    dplyr::mutate(Jurisdiction = "Region") %>%
+    dplyr::mutate(Jurisdiction = ifelse(Filter == 2, "Unincorporated Region", Jurisdiction)) %>%
+    dplyr::mutate(Jurisdiction = ifelse(Filter == 3, "Incorporated Region", Jurisdiction))
+  
+  ofm <- dplyr::bind_rows(ofm,region) %>% dplyr::mutate(Variable = "Population", Data_Type = "Postcensal Estimates") 
+  
+  return(ofm)
+  
+}  
+
+pop.data <- get_ofm_postcensal_population()
+
+
+
